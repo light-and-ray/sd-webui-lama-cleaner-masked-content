e8a91701db9329ce6ea8385d98e17a9155704d1f
Andray <light.and.ray@gmail.com>
2024-03-04 15:16:32 +0400

    allow options in ui

--- lama_cleaner_masked_content/options.py
+++ lama_cleaner_masked_content/options.py
@@ -1,5 +1,4 @@
 from modules import shared
-import modules.scripts
 import gradio as gr
 
 
@@ -7,20 +6,14 @@ def getUpscaler():
     res = shared.opts.data.get("upscaling_upscaler_for_lama_cleaner_masked_content", "ESRGAN_4x")
     return res
 
+lama_cleaner_settings = {
+    'upscaling_upscaler_for_lama_cleaner_masked_content': shared.OptionInfo(
+                "ESRGAN_4x",
+                "Upscaler for lama cleaner masked content",
+                gr.Dropdown,
+                lambda: {"choices": [x.name for x in shared.sd_upscalers]},
+            ),
+}
 
-def on_ui_settings():
-    section = ('upscaling', "Upscaling")
+shared.options_templates.update(shared.options_section(('upscaling', 'Upscaling'), lama_cleaner_settings))
 
-    shared.opts.add_option(
-        "upscaling_upscaler_for_lama_cleaner_masked_content",
-        shared.OptionInfo(
-            "ESRGAN_4x",
-            "Upscaler for lama cleaner masked content",
-            gr.Dropdown,
-            lambda: {"choices": [x.name for x in shared.sd_upscalers]},
-            section=section,
-        ),
-    )
-
-
-modules.scripts.script_callbacks.on_ui_settings(on_ui_settings)
